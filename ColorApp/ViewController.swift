5fa50a2e1a69d486adb6bdbd2316b320438250c8
Nadya <hoppe.nadya@gmail.com>
2023-03-27 22:17:13 +0900

    Setup Actions

--- ColorApp/ViewController.swift
+++ ColorApp/ViewController.swift
@@ -7,13 +7,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    @IBOutlet var colorDisplay: UIView!
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        colorDisplay.layer.cornerRadius = 20
+        colorDisplay.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1.0)
     }
 
+    @IBAction func redSliderAction() {
+        redLabel.text = redSlider.value.formatted(.number.precision(.fractionLength(1)))
+    }
+    
+    @IBAction func greenSliderAction() {
+        greenLabel.text = greenSlider.value.formatted(.number.precision(.fractionLength(1)))
+    }
+    
+    @IBAction func blueSliderAction() {
+        blueLabel.text = blueSlider.value.formatted(.number.precision(.fractionLength(1)))
+    }
+    
 
 }
 
