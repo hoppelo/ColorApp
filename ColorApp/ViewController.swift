7821d084556324bf012954ca2cc70a2d8af3e514
Nadya <hoppe.nadya@gmail.com>
2023-03-27 22:33:43 +0900

    Setup color changing

--- ColorApp/ViewController.swift
+++ ColorApp/ViewController.swift
@@ -20,21 +20,38 @@ final class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         colorDisplay.layer.cornerRadius = 20
-        colorDisplay.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1.0)
+        redSlider.value = 1
+        greenSlider.value = 1
+        blueSlider.value = 1
+
     }
 
     @IBAction func redSliderAction() {
-        redLabel.text = redSlider.value.formatted(.number.precision(.fractionLength(1)))
+        redLabel.text = redSlider.value
+            .formatted(.number.precision(.fractionLength(1)))
+        colorChanging()
     }
     
     @IBAction func greenSliderAction() {
-        greenLabel.text = greenSlider.value.formatted(.number.precision(.fractionLength(1)))
+        greenLabel.text = greenSlider.value
+            .formatted(.number.precision(.fractionLength(1)))
+        colorChanging()
     }
     
     @IBAction func blueSliderAction() {
-        blueLabel.text = blueSlider.value.formatted(.number.precision(.fractionLength(1)))
+        blueLabel.text = blueSlider.value
+            .formatted(.number.precision(.fractionLength(1)))
+        colorChanging()
     }
     
+    private func colorChanging() {
+        colorDisplay.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+    }
 
 }
 
